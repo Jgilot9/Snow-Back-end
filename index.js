db2cb290d9d10e9d026515a5939f7fff209d6e19
v-wang <victorwang920@gmail.com>
2021-12-20 09:24:14 -0500

    edit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 // Controllers start
-const controllerSF = require("./controllers/snowfall");
-app.use("/", controllerSF);
+const controllerSF = require('./controllers/snowfall');
+app.use('/', controllerSF);
 // end
 
-app.listen(app.get("port"), () => {
-  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+app.listen(app.get('port'), () => {
+  console.log(`✅ PORT: ${app.get('port')} 🌟`);
 });
